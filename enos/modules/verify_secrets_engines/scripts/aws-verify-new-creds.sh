377674b6f895f7828fe7ef3c49e3c2f2a85a16a5
tin.vo <tin.vo@hashicorp.com>
2025-03-10 19:45:50 -0700

    debugging aws engine enos test

--- enos/modules/verify_secrets_engines/scripts/aws-verify-new-creds.sh
+++ enos/modules/verify_secrets_engines/scripts/aws-verify-new-creds.sh
@@ -26,8 +26,9 @@ export VAULT_FORMAT=json
 
 echo -e "Configuring Vault AWS \n"
 echo "--------------------${AWS_REGION}"
+aws configure
 USERNAME_TEMPLATE="{{ if (eq .Type \"STS\") }}{{ printf \"${AWS_USER_NAME}-%s-%s\" (random 20) (unix_time) | truncate 32 }}{{ else }}{{ printf \"${AWS_USER_NAME}-%s-%s\" (unix_time) (random 20) | truncate 60 }}{{ end }}"
-"$binpath" write "${MOUNT}/config/root" access_key="${AWS_ACCESS_KEY_ID}" secret_key="${AWS_SECRET_ACCESS_KEY}" region="${AWS_REGION},us-east-1" username_template="${USERNAME_TEMPLATE}"
+"$binpath" write "${MOUNT}/config/root" access_key="${AWS_ACCESS_KEY_ID}" secret_key="${AWS_SECRET_ACCESS_KEY}" region="us-east-1,us-east-2,us-west-1,us-west-2" username_template="${USERNAME_TEMPLATE}"
 echo "---------------------------------"
 
 echo -e "Verifying root config \n"
